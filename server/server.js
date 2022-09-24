5990f2e9adb88ef12182b735acff61960924a1f9
Amy McCabe <mcca0168@umn.edu>
2022-09-24 16:36:12 -0500

    test for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
 });
