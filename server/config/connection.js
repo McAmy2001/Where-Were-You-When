3d633e823d204b7db822b48e71017d25e0ba9970
Amy McCabe <mcca0168@umn.edu>
2022-09-26 17:15:44 -0500

    added seeds.js, only 10 users so far

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/where-were-you-when', {
-  //useNewUrlParser: true,
-  //useUnifiedTopology: true,
-  //useCreateIndex: true,
-  //useFindAndModify: false
+  
 });
 
 module.exports = mongoose.connection;
