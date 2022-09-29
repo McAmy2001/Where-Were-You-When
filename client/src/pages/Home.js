21df00c093ef67fb73f19552986fd076425c94a2
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-29 13:58:02 -0500

    starting to set up home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
+import Auth from "../utils/auth";
+
+// impory queries once they're set up
+// import { useQuery } from '@apollo/client';
+// import { } from '../utils/queries';
+
+const Home = () => {
+  // add useQuery hook here
+
+  const loggedIn = Auth.loggedIn();
+
+  return (
+    <main>
+      <Header />
+      <Main />
+    </main>
+  );
+};
