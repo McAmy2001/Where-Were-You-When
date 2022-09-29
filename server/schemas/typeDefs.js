01942bf35053e58d0c3350cb0e16215c8e68dadd
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-09-29 14:41:06 -0500

    updated typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,21 +18,21 @@ const typeDefs = gql`
     _id: ID
     memoryText: String
     username: String
-    memoryYear: Int
-    memoryMonth: Int
-    memoryDate: Int
+    memoryYear: Number
+    memoryMonth: Number
+    memoryDate: Number
   }
 
   type Query {
     me: User
     users: [User]
-    memory(memoryMonth: Int!, memoryDate: Int!): Memory
+    memory(memoryMonth: Number!, memoryDate: Number!): Memory
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addMemory(memoryText: String!, memoryYear: Int!, memoryMonth: Int!, memoryDate: Int!): Memory
+    addMemory(memoryText: String!, memoryYear: Number!, memoryMonth: Number!, memoryDate: Number!): Memory
   }
 
 `;
