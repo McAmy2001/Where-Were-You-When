77dce3350ae9c6f746d20f2dc5757931f245f7a2
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-29 14:40:32 -0500

    login page set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,46 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Page from "./components/Page";
 
+import Home from "./pages/Home";
+
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
-  const [pageList] = useState([
-    "Main",
-    "Profile",
-    "Login",
-    "Sign Up",
-    "Logout",
-  ]);
-
-  const [activePage] = useState(pageList[0]);
-
   return (
     <ApolloProvider client={client}>
       <div>
         <Header />
 
-        <main>
-          <Page activePage={activePage} />
-        </main>
+        <main>{/* routes to pages here */}</main>
 
         <Footer />
       </div>
---
+++ client/src/pages/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+// import LOGIN_USER mutation here once set up
+// import { useMutation } from "@apollo/client";
+// import {LOGIN_USER} from "../utils/mutations";
+
+import Auth from "../utils/auth";
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  //  use LOGIN_USER mutation once set up
+  // const [login, { error }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <main>
+      <div>
+        <div>
+          <h3>Login</h3>
+          <div>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button>Submit</button>
+            </form>
+
+            {error && <div>Login failed</div>}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
