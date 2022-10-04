ed839c29d14a3282067d088e7c024298a8462124
kaylab78 <kaylabackus78@gmail.com>
2022-10-03 22:09:18 -0500

    Attempt to write a function to handle editing a memory.

--- client/src/components/EditMemoryForm/index.js
+++ client/src/components/EditMemoryForm/index.js
@@ -1,21 +1,54 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { UPDATE_MEMORY } from "../../utils/mutations";
+import { QUERY_MEMORY, QUERY_ME } from "../../utils/queries";
 
 function EditMemoryForm() {
 
+  const [memoryText, setMemoryText] = useState("");
+
+  const [updateMemory, { error }] = useMutation(UPDATE_MEMORY, {
+    update(cache, { data: { updateMemory } }) {
+      try {
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: { ...me, memories: [...me.memories, updateMemory] } },
+        });
+      } catch (e) {
+        console.warn("Memory edited by user.");
+      }
+
+      const { memory } = cache.readQuery({ query: QUERY_MEMORY});
+
+      cache.writeQuery({
+        query: QUERY_MEMORY,
+        data: { memory: [updateMemory, ...memory] }
+      });
+    }
+  });
+
   const handleEditClick = async (event) => {
     event.preventDefault();
-    console.log("Edit clicked");
-    console.log(event);
-    console.log(event.target);
-    console.log(event.target.value);
+
+    try {
+      setMemoryText(event.target.value);
+    } catch (e) {
+      console.error(e.message)
+    }
   }
 
   return (
     <div>
       <form onSubmit={handleEditClick} className="form-flex">
         <label><h4>Edit memory:</h4></label>
-        <textarea className="form-input"></textarea>
+        <textarea 
+          className="form-input"
+          name="memoryText"
+          value={memoryText}
+          onChange={(e) =>
+            setMemoryText(e.target.value)}
+        ></textarea>
         <button type="submit" className="submit-btn">Save Edited Memory</button>
       </form>
     </div>
