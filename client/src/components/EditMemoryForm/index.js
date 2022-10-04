1eef9f672ac9e0c4afb98e0db67f60c848372318
kaylab78 <kaylabackus78@gmail.com>
2022-10-04 14:36:23 -0500

    Trying a new function for updating a memory.

--- client/src/components/EditMemoryForm/index.js
+++ client/src/components/EditMemoryForm/index.js
@@ -5,53 +5,150 @@ import { QUERY_MEMORY, QUERY_ME } from "../../utils/queries";
 
 function EditMemoryForm() {
 
-  const [memoryText, setMemoryText] = useState("");
+  const [formState, setFormState] = useState({memoryText: ''});
+  const { memoryText } = formState;
 
-  const [updateMemory, { error }] = useMutation(UPDATE_MEMORY, {
-    update(cache, { data: { updateMemory } }) {
-      try {
-        const { me } = cache.readQuery({ query: QUERY_ME });
-        cache.writeQuery({
-          query: QUERY_ME,
-          data: { me: { ...me, memories: [...me.memories, updateMemory] } },
-        });
-      } catch (e) {
-        console.warn("Memory edited by user.");
-      }
+  const [updateMemory, { error }] = useMutation(UPDATE_MEMORY);
 
-      const { memory } = cache.readQuery({ query: QUERY_MEMORY});
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value})
+  }
 
-      cache.writeQuery({
-        query: QUERY_MEMORY,
-        data: { memory: [updateMemory, ...memory] }
-      });
-    }
-  });
+  console.log(formState);
 
-  const handleEditClick = async (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
 
     try {
-      setMemoryText(event.target.value);
+      const { data } = await updateMemory({
+        variables: { ...formState }
+      });
+      console.log(data);
     } catch (e) {
-      console.error(e.message)
+      console.error(e);
     }
-  }
+  };
 
   return (
     <div>
-      <form onSubmit={handleEditClick} className="form-flex">
+      <form onSubmit={handleSubmit} className="form-flex">
         <label><h4>Edit memory:</h4></label>
         <textarea 
           className="form-input"
           name="memoryText"
-          onChange={(e) =>
-            setMemoryText(e.target.value)}
+          defaultValue={memoryText}
+          onChange={handleChange}
         ></textarea>
         <button type="submit" className="submit-btn">Save Edited Memory</button>
       </form>
+      {error && <p>Something went wrong.</p>}
     </div>
   )
 }
 
 export default EditMemoryForm;
+
+// function EditMemoryForm() {
+
+//   const [formState, setFormState] = useState({memoryText: ''});
+
+//   const [updateMemory, { error }] = useMutation(UPDATE_MEMORY);
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleEditSubmit = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       const { data } = await updateMemory({
+//         variables: { ...formState }
+//       });
+//     } catch (e) {
+//       console.log(e);
+//     }
+
+//     setFormState({ memoryText: ""});
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleEditSubmit} className="form-flex">
+//         <label><h4>Edit memory:</h4></label>
+//         <textarea 
+//           className="form-input"
+//           name="memoryText"
+//           value={formState.memoryText}
+//           onChange={handleChange}
+//         ></textarea>
+//         <button type="submit" className="submit-btn">Save Edited Memory</button>
+//       </form>
+//       {error && <p>Something went wrong.</p>}
+//     </div>
+//   )
+// }
+
+// export default EditMemoryForm;
+
+// function EditMemoryForm() {
+
+//   const [memoryText, setMemoryText] = useState({memoryText: ''});
+
+//   const [updateMemory, { error }] = useMutation(UPDATE_MEMORY, {
+//     update(cache, { data: { updateMemory } }) {
+//       try {
+//         const { me } = cache.readQuery({ query: QUERY_ME });
+//         cache.writeQuery({
+//           query: QUERY_ME,
+//           data: { me: { ...me, memories: [...me.memories, updateMemory] } },
+//         });
+//       } catch (e) {
+//         console.warn("Memory edited by user.");
+//       }
+
+//       const { memory } = cache.readQuery({ query: QUERY_MEMORY});
+
+//       cache.writeQuery({
+//         query: QUERY_MEMORY,
+//         data: { memory: [updateMemory, ...memory] }
+//       });
+//     }
+//   });
+
+//   const handleEditClick = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       await updateMemory({
+//         variables: {memoryText}
+//       });
+//       setMemoryText(event.target.value);
+//     } catch (e) {
+//       console.error(e.message)
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleEditClick} className="form-flex">
+//         <label><h4>Edit memory:</h4></label>
+//         <textarea 
+//           className="form-input"
+//           name="memoryText"
+//           onChange={(e) =>
+//             setMemoryText(e.target.value)}
+//         ></textarea>
+//         <button type="submit" className="submit-btn">Save Edited Memory</button>
+//       </form>
+//       {error && <p>Something went wrong.</p>}
+//     </div>
+//   )
+// }
+
+// export default EditMemoryForm;
